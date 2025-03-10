78778ac5828ec951b96fded4973c37e4c75b042c
cypherview <support@cypherview.com>
2025-03-10 05:47:42 +0000

    Deploy to test from BitBucket commit 318e9f816fce679806b43f38acb4fe1e0fc73a0c

---
+++ assets/flutter/flutter_service_worker.js
@@ -0,0 +1,222 @@
+'use strict';
+const MANIFEST = 'flutter-app-manifest';
+const TEMP = 'flutter-temp-cache';
+const CACHE_NAME = 'flutter-app-cache';
+
+const RESOURCES = {"main.dart.js": "55c8d3ce71e22e37ee9e4a2dde536b03",
+"flutter_bootstrap.js": "54119d592ead1d1ece8112ddad9ce29f",
+"canvaskit/skwasm_st.js": "d1326ceef381ad382ab492ba5d96f04d",
+"canvaskit/skwasm.js.symbols": "80806576fa1056b43dd6d0b445b4b6f7",
+"canvaskit/canvaskit.js": "6cfe36b4647fbfa15683e09e7dd366bc",
+"canvaskit/canvaskit.wasm": "efeeba7dcc952dae57870d4df3111fad",
+"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
+"canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
+"canvaskit/canvaskit.js.symbols": "68eb703b9a609baef8ee0e413b442f33",
+"canvaskit/skwasm.wasm": "f0dfd99007f989368db17c9abeed5a49",
+"canvaskit/chromium/canvaskit.js": "ba4a8ae1a65ff3ad81c6818fd47e348b",
+"canvaskit/chromium/canvaskit.wasm": "64a386c87532ae52ae041d18a32a3635",
+"canvaskit/chromium/canvaskit.js.symbols": "5a23598a2a8efd18ec3b60de5d28af8f",
+"canvaskit/skwasm_st.js.symbols": "c7e7aac7cd8b612defd62b43e3050bdd",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"assets/AssetManifest.json": "5028272a10e4f0089d7d4351de09e472",
+"assets/FontManifest.json": "63d324d31457d6b10269b00e20e483b3",
+"assets/NOTICES": "3fb13e23fd83b1fdaed68bc37f05c656",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
+"assets/assets/fonts/Ubuntu-Regular.ttf": "84ea7c5c9d2fa40c070ccb901046117d",
+"assets/assets/fonts/Ubuntu-Bold.ttf": "896a60219f6157eab096825a0c9348a8",
+"assets/assets/fonts/Ubuntu-Medium.ttf": "d3c3b35e6d478ed149f02fad880dd359",
+"assets/assets/images/truck-active-sprite-old.png": "b6c5b537db337d565d79183936a6d81e",
+"assets/assets/images/truck-active-sprite.png": "2dd34adca27cec79ba9da90432bb49d2",
+"assets/assets/images/sprite.png": "d656f7df8951f73073e7220e8bd180bd",
+"assets/assets/images/sprite-active.png": "09d0792416fed7893f4b574066daaa2c",
+"assets/assets/icons/visible.svg": "6630d7abfb5b63d0c38e59385e1dc060",
+"assets/assets/icons/menu.svg": "ccd1c1271054fffb0456a26e32113ee1",
+"assets/assets/icons/truck-sprite-active.png": "b6c5b537db337d565d79183936a6d81e",
+"assets/assets/icons/star.svg": "2f6dbc0a52a14997ed20197b43bdb74e",
+"assets/assets/icons/filter.svg": "3b54a3167d944aafa889c9b4b952184a",
+"assets/assets/icons/hidden.svg": "038cde5d5c900ab4889a1034aae7161a",
+"assets/assets/icons/checker.svg": "08354a7093e58bb17ae8409f2c9231a0",
+"assets/assets/icons/asset.svg": "f79483fe25c3581adf1f6effd6996d01",
+"assets/AssetManifest.bin.json": "75d5df2257b867096acf5a46d2336ca3",
+"assets/fonts/MaterialIcons-Regular.otf": "236c5516b45cfaccf58d64935713b413",
+"assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
+"assets/AssetManifest.bin": "aa3be9d19ca44cbe00919fd2e3275965",
+"version.json": "1c3b5a2cf75c8a21333b5d1004a7f382",
+"flutter.js": "76f08d47ff9f5715220992f993002504",
+"index.html": "b293c782bf2f75f159a7b83d7110b560",
+"/": "b293c782bf2f75f159a7b83d7110b560",
+"manifest.json": "5f1150696b7bbee3aa0e32dc8f76ad4d",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea"};
+// The application shell files that are downloaded before a service worker can
+// start.
+const CORE = ["main.dart.js",
+"index.html",
+"flutter_bootstrap.js",
+"assets/AssetManifest.bin.json",
+"assets/FontManifest.json"];
+
+// During install, the TEMP cache is populated with the application shell files.
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  return event.waitUntil(
+    caches.open(TEMP).then((cache) => {
+      return cache.addAll(
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
+    })
+  );
+});
+// During activate, the cache is populated with the temp files downloaded in
+// install. If this service worker is upgrading from one with a saved
+// MANIFEST, then use this to retain unchanged resource files.
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
+    try {
+      var contentCache = await caches.open(CACHE_NAME);
+      var tempCache = await caches.open(TEMP);
+      var manifestCache = await caches.open(MANIFEST);
+      var manifest = await manifestCache.match('manifest');
+      // When there is no prior manifest, clear the entire cache.
+      if (!manifest) {
+        await caches.delete(CACHE_NAME);
+        contentCache = await caches.open(CACHE_NAME);
+        for (var request of await tempCache.keys()) {
+          var response = await tempCache.match(request);
+          await contentCache.put(request, response);
+        }
+        await caches.delete(TEMP);
+        // Save the manifest to make future upgrades efficient.
+        await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        // Claim client to enable caching on first launch
+        self.clients.claim();
+        return;
+      }
+      var oldManifest = await manifest.json();
+      var origin = self.location.origin;
+      for (var request of await contentCache.keys()) {
+        var key = request.url.substring(origin.length + 1);
+        if (key == "") {
+          key = "/";
+        }
+        // If a resource from the old manifest is not in the new cache, or if
+        // the MD5 sum has changed, delete it. Otherwise the resource is left
+        // in the cache and can be reused by the new service worker.
+        if (!RESOURCES[key] || RESOURCES[key] != oldManifest[key]) {
+          await contentCache.delete(request);
+        }
+      }
+      // Populate the cache with the app shell TEMP files, potentially overwriting
+      // cache files preserved above.
+      for (var request of await tempCache.keys()) {
+        var response = await tempCache.match(request);
+        await contentCache.put(request, response);
+      }
+      await caches.delete(TEMP);
+      // Save the manifest to make future upgrades efficient.
+      await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      // Claim client to enable caching on first launch
+      self.clients.claim();
+      return;
+    } catch (err) {
+      // On an unhandled exception the state of the cache cannot be guaranteed.
+      console.error('Failed to upgrade service worker: ' + err);
+      await caches.delete(CACHE_NAME);
+      await caches.delete(TEMP);
+      await caches.delete(MANIFEST);
+    }
+  }());
+});
+// The fetch handler redirects requests for RESOURCE files to the service
+// worker cache.
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  var origin = self.location.origin;
+  var key = event.request.url.substring(origin.length + 1);
+  // Redirect URLs to the index.html
+  if (key.indexOf('?v=') != -1) {
+    key = key.split('?v=')[0];
+  }
+  if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
+    key = '/';
+  }
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
+  if (!RESOURCES[key]) {
+    return;
+  }
+  // If the URL is the index.html, perform an online-first request.
+  if (key == '/') {
+    return onlineFirst(event);
+  }
+  event.respondWith(caches.open(CACHE_NAME)
+    .then((cache) =>  {
+      return cache.match(event.request).then((response) => {
+        // Either respond with the cached resource, or perform a fetch and
+        // lazily populate the cache only if the resource was successfully fetched.
+        return response || fetch(event.request).then((response) => {
+          if (response && Boolean(response.ok)) {
+            cache.put(event.request, response.clone());
+          }
+          return response;
+        });
+      })
+    })
+  );
+});
+self.addEventListener('message', (event) => {
+  // SkipWaiting can be used to immediately activate a waiting service worker.
+  // This will also require a page refresh triggered by the main worker.
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+    return;
+  }
+  if (event.data === 'downloadOffline') {
+    downloadOffline();
+    return;
+  }
+});
+// Download offline will check the RESOURCES for all files not in the cache
+// and populate them.
+async function downloadOffline() {
+  var resources = [];
+  var contentCache = await caches.open(CACHE_NAME);
+  var currentContent = {};
+  for (var request of await contentCache.keys()) {
+    var key = request.url.substring(origin.length + 1);
+    if (key == "") {
+      key = "/";
+    }
+    currentContent[key] = true;
+  }
+  for (var resourceKey of Object.keys(RESOURCES)) {
+    if (!currentContent[resourceKey]) {
+      resources.push(resourceKey);
+    }
+  }
+  return contentCache.addAll(resources);
+}
+// Attempt to download the resource online before falling back to
+// the offline cache.
+function onlineFirst(event) {
+  return event.respondWith(
+    fetch(event.request).then((response) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch((error) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          if (response != null) {
+            return response;
+          }
+          throw error;
+        });
+      });
+    })
+  );
+}
---
+++ chunk-6CRRIC3V.js
@@ -0,0 +1,81 @@
+import{c as fe,m as Se}from"./chunk-ZDMLZBR6.js";import{b as me}from"./chunk-KZ3KZA3Q.js";import{C as v,Da as O,F as ue,K as ce,a as S,b as A,d as $,k as z,ka as de,la as b,pa as ge,w as he}from"./chunk-EMPEPJED.js";import{d as pe}from"./chunk-3HZ37MYL.js";import{$b as te,Ab as H,Bb as q,Cb as D,Dc as k,Ec as C,Jb as m,Kb as w,Ob as J,Pb as W,Rb as V,S as y,Sb as l,Sc as se,T as N,Tb as X,Ub as Y,Uc as re,Vc as le,Wa as g,Wb as R,Xb as Z,Y as p,Yb as E,Yc as ae,Zb as P,Zc as oe,_b as ee,ad as F,bb as Q,cb as j,cd as U,ga as h,gb as L,ha as u,hb as G,ic as ie,ka as _,kb as I,kc as ne,mb as K,nb as x,qa as T,xb as f,yb as c}from"./chunk-HQ3G3WZN.js";import{a as M}from"./chunk-DAQOROHW.js";var ze=class i{userProfileSignal=Q(null);router=p(pe);authService=p(fe);authTokensService=p(me);adminState=p(Se);profile=C(()=>this.userProfileSignal());getUserProfileSignal(){return this.userProfileSignal}getUserProfile(){return this.userProfileSignal()}setUserProfile(a){let e=this.userProfileSignal();e?this.userProfileSignal.set(M(M({},e),a)):this.userProfileSignal.set(a)}clearUserProfile(){this.userProfileSignal.set(null)}logout(){this.authService.signOut({}).subscribe({next:async()=>{this.authTokensService.clearTokens(),this.adminState.clearAllState(),await this.router.navigate(["/auth"])}})}static \u0275fac=function(e){return new(e||i)};static \u0275prov=y({token:i,factory:i.\u0275fac,providedIn:"root"})};var _e=({dt:i})=>`
+.p-splitter {
+    display: flex;
+    flex-wrap: nowrap;
+    border: 1px solid ${i("splitter.border.color")};
+    background: ${i("splitter.background")};
+    border-radius: ${i("border.radius.md")};
+    color: ${i("splitter.color")};
+}
+
+.p-splitter-vertical {
+    flex-direction: column;
+}
+
+.p-splitter-gutter {
+    flex-grow: 0;
+    flex-shrink: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 1;
+    background: ${i("splitter.gutter.background")};
+}
+
+.p-splitter-gutter-handle {
+    border-radius: ${i("splitter.handle.border.radius")};
+    background: ${i("splitter.handle.background")};
+    transition: outline-color ${i("splitter.transition.duration")}, box-shadow ${i("splitter.transition.duration")};
+    outline-color: transparent;
+}
+
+.p-splitter-gutter-handle:focus-visible {
+    box-shadow: ${i("splitter.handle.focus.ring.shadow")};
+    outline: ${i("splitter.handle.focus.ring.width")} ${i("splitter.handle.focus.ring.style")} ${i("splitter.handle.focus.ring.color")};
+    outline-offset: ${i("splitter.handle.focus.ring.offset")};
+}
+
+.p-splitter-horizontal.p-splitter-resizing {
+    cursor: col-resize;
+    user-select: none;
+}
+
+.p-splitter-vertical.p-splitter-resizing {
+    cursor: row-resize;
+    user-select: none;
+}
+
+.p-splitter-horizontal > .p-splitter-gutter > .p-splitter-gutter-handle {
+    height: ${i("splitter.handle.size")};
+    width: 100%;
+}
+
+.p-splitter-vertical > .p-splitter-gutter > .p-splitter-gutter-handle {
+    width: ${i("splitter.handle.size")};
+    height: 100%;
+}
+
+.p-splitter-horizontal > .p-splitter-gutter {
+    cursor: col-resize;
+}
+
+.p-splitter-vertical > .p-splitter-gutter {
+    cursor: row-resize;
+}
+
+.p-splitterpanel {
+    flex-grow: 1;
+    overflow: hidden;
+}
+
+.p-splitterpanel-nested {
+    display: flex;
+    flex-grow: 1;
+    justify-content: center;
+}
+
+.p-splitterpanel .p-splitter {
+    flex-grow: 1;
+    border: 0 none;
+}
+`,xe={root:({props:i})=>["p-splitter p-component","p-splitter-"+i.layout],gutter:"p-splitter-gutter",gutterHandle:"p-splitter-gutter-handle"},ve=(()=>{class i extends ge{name="splitter";theme=_e;classes=xe;static \u0275fac=(()=>{let e;return function(n){return(e||(e=_(i)))(n||i)}})();static \u0275prov=y({token:i,factory:i.\u0275fac})}return i})();var we=["*"],Ee=["panel"],Pe=["container"],Ce=i=>({display:"flex","flex-wrap":"nowrap","flex-direction":i});function be(i,a){i&1&&J(0)}function Me(i,a){if(i&1){let e=W();m(0,"div",6),V("mousedown",function(n){h(e);let s=l().index,r=l();return u(r.onGutterMouseDown(n,s))})("touchstart",function(n){h(e);let s=l().index,r=l();return u(r.onGutterTouchStart(n,s))})("touchmove",function(n){h(e);let s=l(2);return u(s.onGutterTouchMove(n))})("touchend",function(n){h(e);let s=l(2);return u(s.onGutterTouchEnd(n))}),m(1,"div",7),V("keyup",function(n){h(e);let s=l(2);return u(s.onGutterKeyUp(n))})("keydown",function(n){h(e);let s=l().index,r=l();return u(r.onGutterKeyDown(n,s))}),w()()}if(i&2){let e=l(2);f("data-p-gutter-resizing",!1)("data-pc-section","gutter"),g(),c("ngStyle",e.gutterStyle()),f("aria-orientation",e.layout)("aria-valuenow",e.prevSize)("data-pc-section","gutterhandle")}}function Te(i,a){if(i&1&&(m(0,"div",3),x(1,be,1,0,"ng-container",4),w(),x(2,Me,2,6,"div",5)),i&2){let e=a.$implicit,t=a.index,n=l();D(n.panelStyleClass),c("ngClass",n.panelContainerClass())("ngStyle",n.panelStyle),f("data-pc-name","splitter")("data-pc-section","root"),g(),c("ngTemplateOutlet",e),g(),c("ngIf",t!==n.panels.length-1)}}var Le=(()=>{class i extends O{splitter=j(B);nestedState=C(()=>this.splitter());static \u0275fac=(()=>{let e;return function(n){return(e||(e=_(i)))(n||i)}})();static \u0275cmp=L({type:i,selectors:[["p-splitter-panel"]],contentQueries:function(t,n,s){t&1&&ee(s,n.splitter,B,5),t&2&&te()},hostAttrs:[1,"p-splitterpanel"],features:[I],ngContentSelectors:we,decls:1,vars:0,template:function(t,n){t&1&&(X(),Y(0))},dependencies:[F],encapsulation:2,changeDetection:0})}return i})(),B=(()=>{class i extends O{styleClass;panelStyleClass;style;panelStyle;stateStorage="session";stateKey=null;layout="horizontal";gutterSize=4;step=5;minSizes=[];get panelSizes(){return this._panelSizes}set panelSizes(e){if(this._panelSizes=e,this.el&&this.el.nativeElement&&this.panels.length>0){let t=[...this.el.nativeElement.children[0].children].filter(s=>S(s,"p-splitterpanel")),n=[];this.panels.map((s,r)=>{let d=(this.panelSizes.length-1>=r?this.panelSizes[r]:null)||100/this.panels.length;n[r]=d,t[r].style.flexBasis="calc("+d+"% - "+(this.panels.length-1)*this.gutterSize+"px)"})}}onResizeEnd=new T;onResizeStart=new T;containerViewChild;templates;panelChildren;nested=!1;panels=[];dragging=!1;mouseMoveListener;mouseUpListener;touchMoveListener;touchEndListener;size;gutterElement;startPos;prevPanelElement;nextPanelElement;nextPanelSize;prevPanelSize;_panelSizes=[];prevPanelIndex;timer;prevSize;_componentStyle=p(ve);ngOnInit(){super.ngOnInit(),this.nested=this.isNested()}ngAfterContentInit(){this.templates&&this.templates.toArray().length>0&&this.templates.forEach(e=>{switch(e.getType()){case"panel":this.panels.push(e.template);break;default:this.panels.push(e.template);break}}),this.panelChildren&&this.panelChildren.toArray().length>0&&this.panelChildren.forEach(e=>{this.panels.push(e)})}ngAfterViewInit(){if(super.ngAfterViewInit(),U(this.platformId)&&this.panels&&this.panels.length){let e=!1;if(this.isStateful()&&(e=this.restoreState()),!e){let t=[...this.el.nativeElement.children[0].children].filter(s=>S(s,"p-splitterpanel")),n=[];this.panels.map((s,r)=>{let d=(this.panelSizes.length-1>=r?this.panelSizes[r]:null)||100/this.panels.length;n[r]=d,t[r].style.flexBasis="calc("+d+"% - "+(this.panels.length-1)*this.gutterSize+"px)"}),this._panelSizes=n,this.prevSize=parseFloat(n[0]).toFixed(4)}}}resizeStart(e,t,n){this.gutterElement=e.currentTarget||e.target.parentElement,this.size=this.horizontal()?ue(this.containerViewChild.nativeElement):he(this.containerViewChild.nativeElement),n||(this.dragging=!0,this.startPos=this.horizontal()?e instanceof MouseEvent?e.pageX:e.changedTouches[0].pageX:e instanceof MouseEvent?e.pageY:e.changedTouches[0].pageY),this.prevPanelElement=this.gutterElement.previousElementSibling,this.nextPanelElement=this.gutterElement.nextElementSibling,n?(this.prevPanelSize=this.horizontal()?z(this.prevPanelElement,!0):v(this.prevPanelElement,!0),this.nextPanelSize=this.horizontal()?z(this.nextPanelElement,!0):v(this.nextPanelElement,!0)):(this.prevPanelSize=100*(this.horizontal()?z(this.prevPanelElement,!0):v(this.prevPanelElement,!0))/this.size,this.nextPanelSize=100*(this.horizontal()?z(this.nextPanelElement,!0):v(this.nextPanelElement,!0))/this.size),this.prevPanelIndex=t,A(this.gutterElement,"p-splitter-gutter-resizing"),this.gutterElement.setAttribute("data-p-gutter-resizing","true"),A(this.containerViewChild.nativeElement,"p-splitter-resizing"),this.containerViewChild.nativeElement.setAttribute("data-p-resizing","true"),this.onResizeStart.emit({originalEvent:e,sizes:this._panelSizes})}onResize(e,t,n){let s,r,o;n?this.horizontal()?(r=100*(this.prevPanelSize+t)/this.size,o=100*(this.nextPanelSize-t)/this.size):(r=100*(this.prevPanelSize-t)/this.size,o=100*(this.nextPanelSize+t)/this.size):(this.horizontal()?ce(this.el.nativeElement)?s=(this.startPos-e.pageX)*100/this.size:s=(e.pageX-this.startPos)*100/this.size:s=(e.pageY-this.startPos)*100/this.size,r=this.prevPanelSize+s,o=this.nextPanelSize-s),this.prevSize=parseFloat(r).toFixed(4),this.validateResize(r,o)&&(this.prevPanelElement.style.flexBasis="calc("+r+"% - "+(this.panels.length-1)*this.gutterSize+"px)",this.nextPanelElement.style.flexBasis="calc("+o+"% - "+(this.panels.length-1)*this.gutterSize+"px)",this._panelSizes[this.prevPanelIndex]=r,this._panelSizes[this.prevPanelIndex+1]=o)}resizeEnd(e){this.isStateful()&&this.saveState(),this.onResizeEnd.emit({originalEvent:e,sizes:this._panelSizes}),$(this.gutterElement,"p-splitter-gutter-resizing"),$(this.containerViewChild.nativeElement,"p-splitter-resizing"),this.clear()}onGutterMouseDown(e,t){this.resizeStart(e,t),this.bindMouseListeners()}onGutterTouchStart(e,t){e.cancelable&&(this.resizeStart(e,t),this.bindTouchListeners(),e.preventDefault())}onGutterTouchMove(e){this.onResize(e),e.preventDefault()}onGutterTouchEnd(e){this.resizeEnd(e),this.unbindTouchListeners(),e.cancelable&&e.preventDefault()}repeat(e,t,n){this.resizeStart(e,t,!0),this.onResize(e,n,!0)}setTimer(e,t,n){this.clearTimer(),this.timer=setTimeout(()=>{this.repeat(e,t,n)},40)}clearTimer(){this.timer&&clearTimeout(this.timer)}onGutterKeyUp(e){this.clearTimer(),this.resizeEnd(e)}onGutterKeyDown(e,t){switch(e.code){case"ArrowLeft":{this.layout==="horizontal"&&this.setTimer(e,t,this.step*-1),e.preventDefault();break}case"ArrowRight":{this.layout==="horizontal"&&this.setTimer(e,t,this.step),e.preventDefault();break}case"ArrowDown":{this.layout==="vertical"&&this.setTimer(e,t,this.step*-1),e.preventDefault();break}case"ArrowUp":{this.layout==="vertical"&&this.setTimer(e,t,this.step),e.preventDefault();break}default:break}}validateResize(e,t){return!(this.minSizes.length>=1&&this.minSizes[0]&&this.minSizes[0]>e||this.minSizes.length>1&&this.minSizes[1]&&this.minSizes[1]>t)}bindMouseListeners(){this.mouseMoveListener||(this.mouseMoveListener=this.renderer.listen(this.document,"mousemove",e=>{this.onResize(e)})),this.mouseUpListener||(this.mouseUpListener=this.renderer.listen(this.document,"mouseup",e=>{this.resizeEnd(e),this.unbindMouseListeners()}))}bindTouchListeners(){this.touchMoveListener||(this.touchMoveListener=this.renderer.listen(this.document,"touchmove",e=>{this.onResize(e.changedTouches[0])})),this.touchEndListener||(this.touchEndListener=this.renderer.listen(this.document,"touchend",e=>{this.resizeEnd(e),this.unbindTouchListeners()}))}unbindMouseListeners(){this.mouseMoveListener&&(this.mouseMoveListener(),this.mouseMoveListener=null),this.mouseUpListener&&(this.mouseUpListener(),this.mouseUpListener=null)}unbindTouchListeners(){this.touchMoveListener&&(this.touchMoveListener(),this.touchMoveListener=null),this.touchEndListener&&(this.touchEndListener(),this.touchEndListener=null)}clear(){this.dragging=!1,this.size=null,this.startPos=null,this.prevPanelElement=null,this.nextPanelElement=null,this.prevPanelSize=null,this.nextPanelSize=null,this.gutterElement=null,this.prevPanelIndex=null}isNested(){if(this.el.nativeElement){let e=this.el.nativeElement.parentElement;for(;e&&!S(e,"p-splitter");)e=e.parentElement;return e!==null}else return!1}isStateful(){return this.stateKey!=null}getStorage(){if(U(this.platformId))switch(this.stateStorage){case"local":return this.document.defaultView.localStorage;case"session":return this.document.defaultView.sessionStorage;default:throw new Error(this.stateStorage+' is not a valid value for the state storage, supported values are "local" and "session".')}else throw new Error("Storage is not a available by default on the server.")}saveState(){this.getStorage().setItem(this.stateKey,JSON.stringify(this._panelSizes))}restoreState(){let t=this.getStorage().getItem(this.stateKey);return t?(this._panelSizes=JSON.parse(t),[...this.containerViewChild.nativeElement.children].filter(s=>S(s,"p-splitterpanel")).forEach((s,r)=>{s.style.flexBasis="calc("+this._panelSizes[r]+"% - "+(this.panels.length-1)*this.gutterSize+"px)"}),!0):!1}containerClass(){return{"p-splitter p-component":!0,"p-splitter-horizontal":this.layout==="horizontal","p-splitter-vertical":this.layout==="vertical"}}panelContainerClass(){return{"p-splitterpanel":!0,"p-splitterpanel-nested":!0}}gutterStyle(){return this.horizontal()?{width:this.gutterSize+"px"}:{height:this.gutterSize+"px"}}horizontal(){return this.layout==="horizontal"}static \u0275fac=(()=>{let e;return function(n){return(e||(e=_(i)))(n||i)}})();static \u0275cmp=L({type:i,selectors:[["p-splitter"]],contentQueries:function(t,n,s){if(t&1&&(R(s,de,4),R(s,Ee,4)),t&2){let r;E(r=P())&&(n.templates=r),E(r=P())&&(n.panelChildren=r)}},viewQuery:function(t,n){if(t&1&&Z(Pe,5),t&2){let s;E(s=P())&&(n.containerViewChild=s.first)}},hostVars:2,hostBindings:function(t,n){t&2&&H("p-splitterpanel-nested",n.nested)},inputs:{styleClass:"styleClass",panelStyleClass:"panelStyleClass",style:"style",panelStyle:"panelStyle",stateStorage:"stateStorage",stateKey:"stateKey",layout:"layout",gutterSize:[2,"gutterSize","gutterSize",k],step:[2,"step","step",k],minSizes:"minSizes",panelSizes:"panelSizes"},outputs:{onResizeEnd:"onResizeEnd",onResizeStart:"onResizeStart"},features:[ie([ve]),K,I],decls:3,vars:12,consts:[["container",""],[3,"ngClass","ngStyle"],["ngFor","",3,"ngForOf"],["tabindex","-1",3,"ngClass","ngStyle"],[4,"ngTemplateOutlet"],["class","p-splitter-gutter","role","separator","tabindex","-1",3,"mousedown","touchstart","touchmove","touchend",4,"ngIf"],["role","separator","tabindex","-1",1,"p-splitter-gutter",3,"mousedown","touchstart","touchmove","touchend"],["tabindex","0",1,"p-splitter-gutter-handle",3,"keyup","keydown","ngStyle"]],template:function(t,n){t&1&&(m(0,"div",1,0),x(2,Te,3,8,"ng-template",2),w()),t&2&&(q(ne(10,Ce,n.layout==="vertical"?"column":"")),D(n.styleClass),c("ngClass",n.containerClass())("ngStyle",n.style),f("data-pc-name","splitter")("data-p-gutter-resizing",!1)("data-pc-section","root"),g(2),c("ngForOf",n.panels))},dependencies:[F,se,re,le,oe,ae,b],encapsulation:2,changeDetection:0})}return i})(),st=(()=>{class i{static \u0275fac=function(t){return new(t||i)};static \u0275mod=G({type:i});static \u0275inj=N({imports:[B,Le,b,b]})}return i})();export{B as a,st as b,ze as c};
