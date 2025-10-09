e84bc0fa30dde17629ac3a4fdde773500eda87f1
Bitbucket Pipeline <idw2d96x48iqyi7vm5coxf8law7xv9@bitbucket.org>
2025-10-09 12:28:03 +0000

    CHV-849: Deploy Flutter web build 222 from develop branch

--- assets/flutter/flutter_bootstrap.js
+++ assets/flutter/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "905319226"
+    serviceWorkerVersion: "3697474817"
   }
 });
