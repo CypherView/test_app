bdf188d9ef057649f2f27e1a65b8bbc116483ac6
Bitbucket Pipeline <idw2d96x48iqyi7vm5coxf8law7xv9@bitbucket.org>
2026-03-05 17:04:47 +0000

    CHV-849: Deploy Flutter web build 492 from develop branch

--- assets/flutter/flutter_bootstrap.js
+++ assets/flutter/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1350556930" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "857454208" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
